4c3559ee8e0e221a59ebd698c11951e1a7679363
Fedor <fedbut@gmail.com>
2018-09-06 13:23:57 +0300

    add dbwriter

---
+++ writers/db.js
@@ -0,0 +1,66 @@
+const Sequelize = require('sequelize');
+const {
+  dbname, dbusername, dbpass, dbhost,
+} = require('../dbconf');
+
+
+const sequelize = new Sequelize(dbname, dbusername, dbpass, {
+  host: dbhost,
+  dialect: 'postgres',
+
+  pool: {
+    max: 10,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
+
+  operatorsAliases: false,
+});
+
+const { STRING, FLOAT, INTEGER } = Sequelize;
+
+const Results = sequelize.define('results', {
+  runDate: STRING,
+  runTime: STRING,
+  sharpe: FLOAT,
+  exchange: STRING,
+  currency: STRING,
+  asset: STRING,
+  currencyPair: STRING,
+  method: STRING,
+  fullConfig: Sequelize.JSON,
+  stratConfig: Sequelize.JSON,
+  mostProfitabe: FLOAT,
+  biggestLost: FLOAT,
+  winningPercentage: FLOAT,
+  profitFactor: FLOAT,
+  grossProfit: FLOAT,
+  grossLoss: FLOAT,
+  market: FLOAT,
+  relativeProfit: FLOAT,
+  profit: FLOAT,
+  startTime: STRING,
+  endTime: STRING,
+  candleSize: INTEGER,
+  historySize: INTEGER,
+  timespan: STRING,
+  yearlyProfit: FLOAT,
+  relativeYearlyProfit: FLOAT,
+  startPrice: FLOAT,
+  endPrice: FLOAT,
+  trades: INTEGER,
+  startBalance: FLOAT,
+  alpha: FLOAT,
+});
+
+const writeResultToDB = (data) => {
+  sequelize.sync()
+    .then(() => Results.create(data))
+    .then((result) => {
+      console.log('Created DB Entry');
+      console.log(result.toJSON());
+    });
+};
+
+module.exports = writeResultToDB;
