2451ba4aa51615db62f52f682306cb12db429873
Fedor <fedbut@gmail.com>
2018-09-26 11:40:46 +0300

    fix

--- stratConfigs/neataptic.js
+++ stratConfigs/neataptic.js
@@ -2,18 +2,18 @@ const r = require('random-ext');
 
 const neataptic = () => ({
   neataptic: {
-    hiddenLayers: r.integer(2, 6),
-    lookAheadCandles: r.integer(3, 10),
-    iterations: r.integer(50, 300),
-    error: r.float(0.0001, 0.003),
-    momentum: r.float(0.005, 0.03),
+    hiddenLayers: r.integer(6, 2),
+    lookAheadCandles: r.integer(10, 30),
+    iterations: r.integer(300, 50),
+    error: r.float(0.003, 0.0001),
+    momentum: r.float(0.03, 0.005),
     architecture: r.pick(['LSTM', 'Perceptron']),
-    minPercentIncrease: r.integer(1, 50),
-    candlesForShort: r.integerArray(r.integer(2, 6), 99, 10),
-    candlesForLong: r.integerArray(r.integer(2, 6), 99, 10),
-    stopLoss: r.integer(1, 5),
-    RSI: [r.integer(2, 30)],
-    SMA: [r.integer(1, 100), r.integer(101, 1000)],
+    minPercentIncrease: r.integer(50, 1),
+    candlesForShort: r.integerArray(r.integer(6, 2), 99, 10),
+    candlesForLong: r.integerArray(r.integer(6, 2), 99, 10),
+    stopLoss: r.integer(5, 1),
+    RSI: [r.integer(30, 2)],
+    SMA: [r.integer(100, 1), r.integer(1000, 101)],
   },
 });
 
